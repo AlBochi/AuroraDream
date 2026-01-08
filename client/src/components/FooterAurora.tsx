438da14d5fe58a57724dfa7324cf8cc56c4326e1
Al Bochi <albochi@saillent.com>
2026-01-08 15:09:04 -0500

    ✅ Added 4 new social media icons + updated links
        - Added: YouTube, X/Twitter, Threads, LinkedIn
        - Updated Facebook: https://facebook.com/auroradreamsleep
        - Updated Instagram: https://www.instagram.com/auroradream.ca/
        - Added to Contact section and both footers

--- client/src/components/FooterAurora.tsx
+++ client/src/components/FooterAurora.tsx
@@ -18,4 +18,20 @@
     window.open("https://www.linkedin.com/company/aurora-dream", "_blank");
   };
 
+  const handleYouTubeClick = () => {
+    window.open("https://www.youtube.com/@AuroraDreamCa", "_blank");
+  };
+
+  const handleTwitterClick = () => {
+    window.open("https://x.com/AuroraDreamCa", "_blank");
+  };
+
+  const handleThreadsClick = () => {
+    window.open("https://www.threads.net/@auroradream.ca", "_blank");
+  };
+
+  const handleLinkedInClick = () => {
+    window.open("https://www.linkedin.com/company/aurora-dream", "_blank");
+  };
+
 export default FooterAurora;
